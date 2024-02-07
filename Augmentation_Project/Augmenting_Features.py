35007401f9fc1af27cdbf48203dcf1aae32cbaeb
Amanna-Karly-1101 <112589271+AmannaKarly@users.noreply.github.com>
2024-02-07 14:07:53 -0800

    Updated comments for augmented eyes

--- Augmentation_Project/Augmenting_Features.py
+++ Augmentation_Project/Augmenting_Features.py
@@ -1,14 +1,6 @@
-import PIL.Image
 import cv2
-import numpy
 import numpy as np
 import os
-import math
-import skimage
-import scipy
-import dlib
-from PIL import Image, ImageOps
-from PIL import ImageDraw
 
 '''
 Name: Rhiannon Barber
@@ -135,18 +127,74 @@ def create_nose_mask(img, feature_points, width_margin_factor, height_margin_fac
     return mask
 
 
-def create_eye_mask(img, feature_points, width_margin_factor, height_margin_factor):
-    mask = np.zeros_like(img)
-    x_min, y_min = np.min(feature_points, axis=0).astype(int)
-    x_max, y_max = np.max(feature_points, axis=0).astype(int)
-    width_margin = int((x_max - x_min) * width_margin_factor)
-    height_margin = int((y_max - y_min) * height_margin_factor)
-    x_min = max(x_min - width_margin, 0)
-    y_min = max(y_min - height_margin, 0)
-    x_max = min(x_max + width_margin, img.shape[1])
-    y_max = min(y_max + height_margin, img.shape[0])
-    mask[y_min:y_max, x_min:x_max] = 255
+'''
+This function creates a mask where the area around the mask is white, to indicate the ROI. 
+The rest of the mask is black, it can then be used in the blending.
+The mask uses the land mark points to define the region of the mask. 
+'''
+
+
+def create_eye_mask(eye_landmarks1, eye_landmarks2, img):
+    # mask 1, makes mask for reduced eye
+    mask = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.float32)
+    cv2.fillConvexPoly(mask, np.int32(eye_landmarks1), (1.0, 1.0, 1.0))
+    cv2.fillConvexPoly(mask, np.int32(eye_landmarks2), (1.0, 1.0, 1.0))
+    mask = 255*np.uint8(mask)
+
+    # Apply close operation to improve mask
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40, 40))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     return mask
+
+
+'''
+This function puts the image in the mask and puts an image in the inverse mask. For mask it puts a reduced image 
+of the eyes in the mask. While for mask2 it puts a blended image in the mask2 and 
+the normal image in each ones inverse mask.
+'''
+
+
+def multiply_eye_mask(mask, inverse_mask, eye, face):
+    # Multiply eyes and face by the mask
+    just_eye = cv2.multiply(mask, eye)
+    just_face = cv2.multiply(inverse_mask, face)
+    result = just_face + just_eye  # Add face and eye
+    return result
+
+
+# Calculates inverse mask and reduces size of image. Then multiplies mask and image together.
+def make_img(img, mask, face_landmarks, eyebrow_landmarks, face_img, type_mask):
+    inverse_mask = cv2.bitwise_not(mask)  # Calculate inverse mask
+    # Convert masks to float to perform blending
+    mask = mask.astype(float)/255
+    inverse_mask = inverse_mask.astype(float)/255
+    img = img.astype(np.uint8)  # Apply color mapping for the eyes
+    # Find amount to reduce image
+    face_width = face_landmarks[0, 1]
+    eyebrow_height = eyebrow_landmarks[3, 1]
+    h, w, c = img.shape
+    height_reduced = int((h-eyebrow_height)/32)
+    width_reduced = int((w-face_width)/8)
+    # Convert eyes and face to 0-1 range
+    face = img.copy()
+    head = img.astype(float)/255
+    if type_mask == 'mask':
+        # Resize
+        eye = cv2.copyMakeBorder(img, height_reduced, height_reduced*3, width_reduced, width_reduced, cv2.BORDER_CONSTANT)
+        eye = cv2.resize(eye, (w, h))
+        eye = eye.astype(float)/255
+        # Multiply eyes and face by the mask
+        result = multiply_eye_mask(mask, inverse_mask, eye, face_img)
+    else:
+        # Resize
+        face = cv2.resize(face, (w*2, h*2))
+        face = cv2.copyMakeBorder(face, 0, 0, int(width_reduced*0.3), int(width_reduced*0.3), cv2.BORDER_CONSTANT)
+        face = cv2.resize(face, (w, h))
+        face = cv2.GaussianBlur(face, (0, 0), 8)
+        face = face.astype(float)/255
+        # Multiply eyes and face by the mask
+        result = multiply_eye_mask(mask, inverse_mask, face, head)
+    return result
 
 
 def main():
@@ -155,7 +203,6 @@ def main():
     image_directory = get_dir('original_images')
     augmented_directory = get_dir('augmented_images')
     nose_scale_factor = 1.25
-    eye_scale_factor = 0.80
 
     if not os.path.exists(augmented_directory):
         os.makedirs(augmented_directory)
@@ -169,6 +216,7 @@ def main():
         original_img = img.copy()
         img = resize_nose(img, facial_features, image_id, feature_to_int, nose_scale_factor)
         nose_landmarks = load_feature_landmarks(facial_features, image_id, feature_to_int, 'nose')
+
         # the margins here for the mask affect where the bounds of the box are.
         nose_mask = create_nose_mask(original_img, nose_landmarks, width_margin_factor=0.4, height_margin_factor=0.1)
 
@@ -191,6 +239,7 @@ def main():
         # saved to the augmented_images dir.
         augmented_img_name = f"augmented_{os.path.basename(img_path)}"
         augmented_img_path = os.path.join(augmented_directory, augmented_img_name)
+        cv2.imwrite(augmented_img_path, img)
 
         # START TO EYE AUGMENTATION #
         # Get landmarks for eyes and other facial features
@@ -199,109 +248,60 @@ def main():
         face_landmarks = load_feature_landmarks(facial_features, image_id, feature_to_int, 'jawline')
         nose_landmarks = load_feature_landmarks(facial_features, image_id, feature_to_int, 'nose')
 
-        # Delete eye_landmarks to make shape of numpy arrays the same
-        left_eye, right_eye = numpy.array_split(eye_landmarks, 2)  # Delete the second outward landmark
-        left_brow, right_brow = numpy.array_split(eyebrow_landmarks, 2)
+        # Delete eye landmarks to make shape of numpy arrays the same
+        left_eye, right_eye = np.array_split(eye_landmarks, 2)  # Delete the second outward landmark
+        left_brow, right_brow = np.array_split(eyebrow_landmarks, 2)
         l_eye, r_eye = left_eye.copy(), right_eye.copy()
 
         # Delete outermost eyebrow landmark to
         # make upper eye and eyebrow landmarks number the same
-        left_brow = numpy.delete(left_brow, 3, 0)
-        right_brow = numpy.delete(right_brow, 1, 0)
+        left_brow = np.delete(left_brow, 3, 0)
+        right_brow = np.delete(right_brow, 1, 0)
 
         # Delete bottom two eye landmarks
-        left_eye = numpy.delete(left_eye, 4, 0)
-        left_eye = numpy.delete(left_eye, 4, 0)
-        right_eye = numpy.delete(right_eye, 4, 0)
-        right_eye = numpy.delete(right_eye, 4, 0)
+        left_eye = np.delete(left_eye, 4, 0)
+        left_eye = np.delete(left_eye, 4, 0)
+        right_eye = np.delete(right_eye, 4, 0)
+        right_eye = np.delete(right_eye, 4, 0)
 
         # Expand eye_landmarks by half the distance from eyes to eyebrows
         result1 = (left_eye - left_brow)
         result2 = (right_eye - right_brow)
         result1[0, 0] = -(face_landmarks[0, 0] - left_eye[0, 0])/2  # make horizontal mask wider
         result2[3, 0] = -((face_landmarks[16, 0] - right_eye[3, 0])/2)  # make horizontal mask wider
-        l_lower_expand = (result1[3, 1])  # Get the largest distance from results
-        r_lower_expand = (result1[3, 1])   # Get the largest distance from results
         result1[3, 0] = (left_eye[3, 0] - nose_landmarks[0, 0])/2   # Expand eye near nose horizontal
         result2[0, 0] = -(nose_landmarks[0, 0] - right_eye[0, 0])/2  # Expand eye near nose horizontal
+        l_lower_expand = (result1[3, 1])  # Get the largest distance from results
+        r_lower_expand = (result1[3, 1])   # Get the largest distance from results
         result1, result2 = result1 / 2, result2 / 2
         # Insert bottom eye landmarks and expand downward
-        # result1 = numpy.append(result1, [[-(face_landmarks[0, 0] - left_eye[0, 0])/2, -l_lower_expand]], axis=0)
-        result1 = numpy.append(result1, [[-(face_landmarks[0, 0] - left_eye[0, 0])/2, -l_lower_expand/2]], axis=0)
-        result1 = numpy.insert(result1, 4, [-result1[0, 0], -l_lower_expand], axis=0)
-        result2 = numpy.append(result2, [[result2[3, 1], -r_lower_expand]], axis=0)
-        result2 = numpy.insert(result2, 4, [-(face_landmarks[16, 0] - right_eye[3, 0])/2, -r_lower_expand/2], axis=0)
+        result1 = np.append(result1, [[-(face_landmarks[0, 0] - left_eye[0, 0])/2, -l_lower_expand/2]], axis=0)
+        result1 = np.insert(result1, 4, [-result1[0, 0], -l_lower_expand], axis=0)
+        result2 = np.append(result2, [[result2[3, 1], -r_lower_expand]], axis=0)
+        result2 = np.insert(result2, 4, [-(face_landmarks[16, 0] - right_eye[3, 0])/2, -r_lower_expand/2], axis=0)
         # Calculate expanded result into eye_landmarks
         eye_landmarks1, eye_landmarks2 = l_eye - result1,  r_eye - result2
 
         # mask 1, makes mask for reduced eye
-        mask = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.float32)
-        cv2.fillConvexPoly(mask, np.int32(eye_landmarks1), (1.0, 1.0, 1.0))
-        cv2.fillConvexPoly(mask, np.int32(eye_landmarks2), (1.0, 1.0, 1.0))
-        mask = 255*np.uint8(mask)
+        mask = create_eye_mask(eye_landmarks1, eye_landmarks2, img)
         # mask 2, makes mask for blurred background of eye
         eye_landmarks1[0, 0] = eye_landmarks1[0, 0]+((face_landmarks[0, 0] - eye_landmarks1[0, 0])/2)  # make horizontal mask wider
         eye_landmarks2[3, 0] = eye_landmarks2[3, 0]+((face_landmarks[16, 0] - eye_landmarks2[3, 0])/2)  # make horizontal mask wider
         eye_landmarks1[5, 0] = eye_landmarks1[5, 0]-(face_landmarks[16, 0] - eye_landmarks2[3, 0])  # make horizontal mask wider
         eye_landmarks2[4, 0] = eye_landmarks2[4, 0]+(face_landmarks[16, 0] - eye_landmarks2[3, 0])  # make horizontal mask wider
-        # print(eye_landmarks1[0, 0])
-        mask2 = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.float32)
-        cv2.fillConvexPoly(mask2, np.int32(eye_landmarks1), (1.0, 1.0, 1.0))
-        cv2.fillConvexPoly(mask2, np.int32(eye_landmarks2), (1.0, 1.0, 1.0))
-        mask2 = 255*np.uint8(mask2)
+        mask2 = create_eye_mask(eye_landmarks1, eye_landmarks2, img)
 
-        # Apply close operation to improve mask
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40, 40))
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-        mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel)
-        blur = (3*int(result1[0, 0]))
         # Blur the mask to obtain natural result
+        blur = (3*int(result1[0, 0]))
         if not(blur % 2):
             blur = blur + 1
         mask = cv2.GaussianBlur(mask, (blur, blur), 99)
         mask2 = cv2.GaussianBlur(mask2, (99, 99), 32)
-        # cv2.imshow("", mask2)
-        # cv2.waitKey(0)
 
-        # Calculate inverse mask
-        inverse_mask = cv2.bitwise_not(mask)
-        inverse_mask2 = cv2.bitwise_not(mask2)
+        # Put masks into image
+        result = make_img(img, mask2, face_landmarks, eyebrow_landmarks, img, "mask2")
+        result = make_img(img, mask, face_landmarks, eyebrow_landmarks, result, "mask")
 
-        # Convert masks to float to perform blending
-        mask = mask.astype(float)/255
-        inverse_mask = inverse_mask.astype(float)/255
-        mask2 = mask2.astype(float)/255
-        inverse_mask2 = inverse_mask2.astype(float)/255
-
-        # Apply color mapping for the eyes
-        img = img.astype(np.uint8)
-        face_width = face_landmarks[0, 1]
-        eyebrow_height = eyebrow_landmarks[3, 1]
-        h, w, c = img.shape
-        height_reduced = int((h-eyebrow_height)/32)
-        width_reduced = int((w-face_width)/8)
-        eye = cv2.copyMakeBorder(img, height_reduced, height_reduced*3, width_reduced, width_reduced, cv2.BORDER_CONSTANT)
-        eye = cv2.resize(eye, (w, h))
-        face = img.copy()
-        face = cv2.resize(face, (w*2, h*2))
-        face = cv2.copyMakeBorder(face, 0, 0, int(width_reduced*0.3), int(width_reduced*0.3), cv2.BORDER_CONSTANT)
-        face = cv2.resize(face, (w, h))
-        face = cv2.GaussianBlur(face, (0, 0), 8)
-
-        # Convert lips and face to 0-1 range
-        eye = eye.astype(float)/255
-        face = face.astype(float)/255
-        head = img.astype(float)/255
-
-        # Multiply eyes and face by the mask
-        just_eye = cv2.multiply(mask2, face)
-        just_face = cv2.multiply(inverse_mask2, head)
-        result = just_face + just_eye  # Add face and eye
-
-        # Multiply eyes and face by the mask2
-        just_eye = cv2.multiply(mask, eye)
-        just_face = cv2.multiply(inverse_mask, result)
-        result = just_face + just_eye  # Add face and eye
         # Show result
         result = cv2.normalize(result, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
         cv2.imwrite(augmented_img_path, result)
